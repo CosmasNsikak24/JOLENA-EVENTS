5bd6741cca2a0a42a97249526ea2fb6bc2117eab
Cosmas2004 <cosmasnsikakabasi@gmail.com>
2023-03-29 23:05:32 +0100

    init

---
+++ js/main.js
@@ -0,0 +1,65 @@
+ const responsive={
+    0:{
+        items:1
+    },
+    350:{
+        items:1
+    },
+    500:{
+        items:2
+    },
+    1000:{
+        items:3
+    },
+    1075:{
+        items:4
+    }
+
+}
+
+
+
+$(document).ready(function(){
+
+ $nav= $('.nav');
+ $toggleCollapse =$('.toggle-collapse');
+
+// click event on toggle menu
+$toggleCollapse.click(function(){
+    $nav.toggleClass('collapse');
+})
+
+// fixed header
+$(window).scroll(function(){
+    if($(this).scrollTop() > 100){
+        $(".navbar").addClass("fixed");
+    }
+    else{
+        $(".navbar").removeClass("fixed");
+    }
+})
+
+// owl-carousel for blog
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    dots:false,
+    nav:true,
+    navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+    responsive:responsive
+});
+
+
+// click to scroll top
+$('.move-up span').click(function(){
+    $('html,body').animate({
+        scrollTop:0
+    },2000);
+})
+
+// AOS INSTANCE
+AOS.init()
+
+
+});
---
+++ js/mainnewtemp.js
@@ -0,0 +1,39 @@
+$(window).on("load",function(){
+
+    // nav toggle
+    $(".hamburger-btn").click(function(){
+        $(".header .nav").slideToggle();
+    })
+    $(".header .nav").click(function(){
+        if($(window).width() < 768){
+            $(".header .nav").slideToggle();
+        }
+    })
+
+
+    // fixed header
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100){
+            $(".header").addClass("fixed");
+        }
+        else{
+            $(".header").removeClass("fixed");
+        }
+    })    
+
+    // home section slideshow
+    let slideIndex = $(".slide.active").index();
+    const slideLen = $(".slide").length;
+
+    function slideshow(){
+        $(".slide").removeClass("active").eq(slideIndex).addClass("active");
+        if(slideIndex == slideLen-1){
+            slideIndex = 0;
+        }
+        else{
+            slideIndex++;
+        }
+        setTimeout(slideshow,5000);
+    }
+    slideshow();
+})
